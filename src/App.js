18dd8400d6fd63e509b03d81a628fde47a1ea87d
gratus2806 <gratusbaptist2806@gmail.com>
2019-10-11 15:49:27 +0530

    circle square and softcorner

---
+++ src/App.js
@@ -0,0 +1,241 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  
+  return (
+    <div >
+      {/* <<<<<<<<<<<<<Circle>>>>>>>>>>>>>>>>>>>>> */}
+      <div class='container'>
+        <div style={{textAlign:"center"}}>
+          <h1 >Small Circle</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-small'>
+             
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-small' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-circle-small-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Medium Circle</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-medium'>
+             
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-medium' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-circle-medium-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Large Circle</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-large'>
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-large' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-circle-large-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Extra Large Circle</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-extraLarge' >
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-circle-extraLarge' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-circle-extraLarge-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <<<<<<<<<<<<<<<<<<<<SQUARE>>>>>>>>>>>>>>>>>>>>> */}
+      <div class='container'>
+        <div style={{textAlign:"center"}}>
+          <h1 >Small Square</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-small'>
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-small' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-square-small-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Medium Square</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-medium'>
+             
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-medium' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-square-medium-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >large Square</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-large'>
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-large' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-square-large-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Extra large Square</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-extraLarge'>
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-extraLarge' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-square-extraLarge-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Full Screen Square</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-fullScreen'  >
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-square-fullScreen' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-square-fullScreen-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <<<<<<<<<<<<<<<<<<<<<<SoftCorner>>>>>>>>>>>>> */}
+      <div class='container'>
+        <div style={{textAlign:"center"}}>
+          <h1 >Small Circle</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-small'>
+             
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-small' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-softCorner-small-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Medium softCorner</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-medium'>
+             
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-medium' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-softCorner-medium-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Large softCorner</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-large'>
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-large' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-softCorner-large-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+        <div style={{textAlign:"center"}}>
+          <h1 >Extra Large softCorner</h1>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-extraLarge' >
+              
+            </div>
+          </div>
+        </div>
+        <div class='row'>
+          <div class='col-sm-12 col-style'>
+            <div class='image-softCorner-extraLarge' style={{border:"none",boxShadow:"none"}}>
+              <img class="image-softCorner-extraLarge-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
