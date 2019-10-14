9fd7d7a17255b9f5b6de006c8cdb964e2005b08c
gratus2806 <gratusbaptist2806@gmail.com>
2019-10-14 23:03:44 +0530

    bug fixing

--- src/layout/image_galary/index.js
+++ src/layout/image_galary/index.js
@@ -5,151 +5,145 @@ import YouTube from 'react-youtube';
 import Carousel from 'react-bootstrap/Carousel';
 
 class Image_galary extends React.Component{
-  render(){
+  constructor(props){
+    super(props)
+      this.state={}
+      if(window.outerWidth>=1200){
+        this.state={
+            visiblity:"none"
+        }
+      } else{
+        this.state={
+            visiblity:"block"
+        } 
+      }
+
+}
+
+render(){
+  let visiblity = this.state.visiblity; 
       return(
-        <div>
-        <div className='container-fluid'>
-             {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<image Circular galary>>>>>>>>>>>>>>>>>>>>>></image> */}
-             <div className="row">
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-circle">
-                        <ul className="ul_imageStyle-circle-empty">
-                            <li className="column-circle-image ">
-                            <img className="upload_image_style_circle" src="https://png.pngtree.com/svg/20170823/upload_video_162306.png" alt="Smiley face" />
-                              <img className="image-circle-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />  
-                            </li>
-                            <li className="column-circle-image">
-                              <img className="image-circle-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-              <div className="row">
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-circle">
-                        <ul className="ul_imageStyle-circle">
-                            <li className="column-circle-image">
-                                <a href="/doodles">
-                                    <img className="image-circle-style" src="https://i.imgur.com/ZerwVp3.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                            <li className="column-circle-image">
-                                <a href="/character">
-                                    <img className="image-circle-style" src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            {/* <<<<<<<<<<<<<<<<<<<<image Soft corner>>>>>>>>>>>>>>>>>>></> */}
-
-            <div className="row">
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-softCorner">
-                        <ul className="ul_imageStyle-softCorner-empty">
-                            <li className="column-softCorner-image ">
-                            <img className="upload_image_style_softCorner" src="https://png.pngtree.com/svg/20170823/upload_video_162306.png" alt="Smiley face" />
-                              <img className="image-softCorner-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />  
-                            </li>
-                            <li className="column-softCorner-image">
-                              <img className="image-softCorner-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-              <div className="row">
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-softCorner">
-                        <ul className="ul_imageStyle-softCorner">
-                            <li className="column-softCorner-image">
-                                <a href="/doodles">
-                                    <img className="image-softCorner-style" src="https://i.imgur.com/ZerwVp3.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                            <li className="column-softCorner-image">
-                                <a href="/character">
-                                    <img className="image-softCorner-style" src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-
-            {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<sharp edges image gal>>>>>>>>>>>>>>>>>>> */}
-
-            <div className="row">
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-sharpEdge">
-                        <ul className="ul_imageStyle-sharpEdge-empty">
-                            <li className="column-sharpEdge-image ">
-                            <img className="upload_image_style_sharpEdge" src="https://png.pngtree.com/svg/20170823/upload_video_162306.png" alt="Smiley face" />
-                              <img className="image-sharpEdge-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />  
-                            </li>
-                            <li className="column-sharpEdge-image">
-                              <img className="image-sharpEdge-style" src="https://i.imgur.com/dm4Rwzw.gif" alt="Smiley face" />
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-              <div className="row row_style" >
-                <div className="col-lg-12 col-xs-12 col-sm-12 " >
-                    <div className="row imageRow-sharpEdge">
-                        <ul className="ul_imageStyle-sharpEdge">
-                            <li className="column-sharpEdge-image">
-                                <a href="/doodles">
-                                    <img className="image-sharpEdge-style" src="https://i.imgur.com/ZerwVp3.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                            <li className="column-sharpEdge-image">
-                                <a href="/character">
-                                    <img className="image-sharpEdge-style" src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg" alt="Smiley face" />
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-              {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<one below other image>>>>>>>>>>>>>>>>>>>>>>></one> */}
-
-              <div className="row row-after"> 
-              {/* <div className="column-image-oneBelow-other-style">
-                <ul className="image-oneBelow-other-ul-style">
-                  <li>
-                    <div className="image-oneBelow-other"  style={{width:"80%",marginLeft:"2px"}} >
-                    <a href="/doodles">
-                      <i className="image-oneBelow-other-button fa fa-play fa-2x "></i>
-                    </a>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="image-oneBelow-other"  style={{width:"80%",marginLeft:"2px"}} >
-                    </div>
-                  </li>
-                  
+        <div className="layout">
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="upload_video_style_circle" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-circle">
+                        </div>
+                    </li>
                 </ul>
-              </div> */}
             </div>
-            <div className="row row-after"> 
-              <div className="column-image-oneBelow-other-style">
-                <ul className="image-oneBelow-other-ul-style">
-                  <li>
-                    <img className="image-oneBelow-other-style" src="https://i.imgur.com/ZerwVp3.jpg" style={{width:"80%",marginLeft:"2px"}} />
-                  </li>
-                  <li>
-                    <img className="image-oneBelow-other-style" src="https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg" style={{width:"80%",marginLeft:"2px"}} />
-                  </li>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="image-grid-circle-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="image-grid-circle-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
                 </ul>
-              </div>
+            </div>
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    
+                    <li>
+                        <div className="image-grid-softCorner">
+                        <img className="upload_video_style_softCorner" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-softCorner">
+                            {/* <img className="image-grid-softCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    
+                    <li>
+                        <div className="image-grid-softCorner">
+                            <img className="image-grid-softCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-softCorner">
+                            <img className="image-grid-softCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
             </div>
 
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                        <img className="upload_video_style_sharpCorner" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            {/* //<img className="image-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            <img className="image-grid-sharpCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            <img className="image-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none" }}>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                        <img className="upload_video_style_one-below-other" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            {/* //<img className="image-grid-one-below-other-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none" ,padding:"0p"}}>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            <img className="image-grid-one-below-other-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            <img className="image-grid-one-below-other-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            
+            
         </div>
-    </div>
       )
   }
 }
--- src/layout/video_modal/index.js
+++ src/layout/video_modal/index.js
@@ -7,82 +7,144 @@ import YouTube from 'react-youtube';
 class Video_modal extends React.Component{
     constructor(props){
         super(props)
-        this.state={
-        
-        }
-        this.myRefs = React.createRef();
+          this.state={}
+          if(window.outerWidth>=1200){
+            this.state={
+                visiblity:"none"
+            }
+          } else{
+            this.state={
+                visiblity:"block"
+            } 
+          }
+
     }
-    // openModal = () => {
-    //     const node = this.myRefs.current;
-    //     console.log("hiiii",node)
-    //     videourl = node.getAttribute('data-src')
-        
-
-
-    // };
-    componentDidMount() {
-        window.$(document).ready(function() {
-            var $videoSrc;  
-            window.$('.video-btn').click(function() {
-                $videoSrc = window.$(this).data( "src" );
-            });
-            console.log($videoSrc);
-            // when the modal is opened autoplay it  
-            window.$('#myModal').on('shown.bs.modal', function (e) {
-            // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-            window.$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
-            })
-            // stop playing the youtube video when I close the modal
-            window.$('#myModal').on('hide.bs.modal', function (e) {
-                // a poor man's stop video
-                window.$("#video").attr('src',$videoSrc); 
-            }) 
-            // document ready  
-            });
-            
-      }
 
     render(){
-        
+      let visiblity = this.state.visiblity;  
     
   
     return(
-        <div class="container">
-            <h1>Play YouTube or Vimeo Videos in Bootstrap 4 Modal</h1>
-
-            {/* <!-- Button trigger modal --> */}
-            <div className='row'>
-                <div className='video-softCorner-rectangle' >
-                {/* <button type="button" class="video-btn" data-toggle="modal" data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal">
-                     Play Video 1 - autoplayss
-                </button> */}
-                <a type="button" class="video-btn" data-toggle="modal" data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal">
-                <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "31%",top: "45%",zIndex:"999"}}></i>
-                <img className="video-softCorner-rectangle-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
-                </a>
-                </div>
+        <div className="layout">
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="upload_video_style_circle" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-circle">
+                        </div>
+                    </li>
+                </ul>
             </div>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="image-grid-circle-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-circle">
+                            <img className="image-grid-circle-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    
+                    <li>
+                        <div className="image-grid-softCorner">
+                        <img className="upload_video_style_softCorner" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-softCorner">
+                            {/* <img className="image-grid-softCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    
+                    <li>
+                        <div className="image-grid-softCorner">
+                            <img className="image-grid-softCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-softCorner">
+                            <img className="image-grid-softCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                        <img className="upload_video_style_sharpCorner" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            {/* //<img className="image-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none"}}>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            <img className="image-grid-sharpCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-sharpCorner">
+                            <img className="image-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div className="row" style={{display:visiblity}}>
+                <ul style={{listStyleType:"none" }}>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                        <img className="upload_video_style_one-below-other" src="https://dezov.s3.amazonaws.com/media/upload-icon-png427-4f1f-8b21-dedb5c507065.png" alt="Smiley face" />
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            {/* //<img className="image-grid-one-below-other-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className="row">
+                <ul style={{listStyleType:"none" ,padding:"0p"}}>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            <img className="image-grid-one-below-other-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="image-grid-one-below-other">
+                            <img className="image-grid-one-below-other-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
             
-            {/* <!-- Modal --> */}
-            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-
-                        <div class="modal-body">
-
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                        {/* <!-- 16:9 aspect ratio --> */}
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always" allow="autoplay"></iframe>
-                        </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            
         </div>
      
      )
