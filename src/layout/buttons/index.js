699451de286f967c0b4631b7c461edae987278d2
gratus2806 <gratusbaptist2806@gmail.com>
2019-10-17 12:05:24 +0530

    changes done

--- src/layout/buttons/index.js
+++ src/layout/buttons/index.js
@@ -12,19 +12,19 @@ class Buttons extends React.Component{
                 <div className="row">
                   <div className ="align-button " >
                     <h2>sharp Buttons Filled</h2>
-                    <button type="button" class="btn btn-primary button-fill-sharp-style">Primary</button>
+                    <button type="button" class="btn btn-primary button-shadow button-fill-sharp-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>circle Buttons</h2>
-                    <button type="button" class="btn btn-primary button-fill-circle-style">Primary</button>
+                    <button type="button" class="btn btn-primary button-shadow button-fill-circle-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>soft edge Buttons</h2>
-                    <button type="button" class="btn btn-primary button-fill-softEdge-style">Primary</button>
+                    <button type="button" class="btn btn-primary button-shadow button-fill-softEdge-style">Primary</button>
                   </div>
                 </div>
 
@@ -32,19 +32,19 @@ class Buttons extends React.Component{
                 <div className="row">
                   <div className ="align-button " >
                     <h2>sharp edge Buttons</h2>
-                    <button type="button" class="btn btn-outline-primary waves-effect button-outlined-sharpEdge-style">Primary</button>
+                    <button type="button" class="btn btn-outline-primary button-shadow waves-effect button-outlined-sharpEdge-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>circle outlined Buttons</h2>
-                    <button type="button" class="btn btn-outline-primary waves-effect button-outlined-circle-style">Primary</button>
+                    <button type="button" class="btn btn-outline-primary button-shadow waves-effect button-outlined-circle-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>soft edge Buttons</h2>
-                    <button type="button" class="btn btn-outline-primary waves-effect button-outlined-softEdge-style">Primary</button>
+                    <button type="button" class="btn btn-outline-primary button-shadow waves-effect button-outlined-softEdge-style">Primary</button>
                   </div>
                 </div>
 
@@ -53,43 +53,43 @@ class Buttons extends React.Component{
                   <div className ="align-button " >
                     <h2>sharp edge Buttons</h2>
                     {/* <button class="btn button-gradient-sharpEdge-style">Primary</button> */}
-                    <button class="btn peach-gradient button-gradient-sharpEdge-style">Peach</button>
+                    <button class="btn peach-gradient button-shadow button-gradient-sharpEdge-style">Peach</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>circular edge Buttons</h2>
-                    <button class="btn btn peach-gradient button-gradient-circle-style">Primary</button>
+                    <button class="btn btn peach-gradient button-shadow button-gradient-circle-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                     <h2>soft edge Buttons</h2>
-                    <button class="btn btn peach-gradient button-gradient-softEdge-style">Primary</button>
+                    <button class="btn btn peach-gradient button-shadow button-gradient-softEdge-style">Primary</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> just text buttons</h2>
-                  <button type="button"  class="btn btn-link button-link-style">text Button</button>
+                  <button type="button"  class="btn btn-link button-shadow button-link-style">text Button</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> sharp icon AND text </h2>
-                    <button class="btn btn-primary button-icon-text-sharpEdge-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                    <button class="btn btn-primary button-shadow button-icon-text-sharpEdge-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> circular icon AND text </h2>
-                    <button class="btn btn-primary button-icon-text-rounded-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                    <button class="btn btn-primary button-shadow button-icon-text-rounded-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> soft edge</h2>
-                    <button class="btn btn-primary button-icon-text-softEdge-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
+                    <button class="btn btn-primary button-shadow button-icon-text-softEdge-style"><i class="fa fa-facebook-f mr-2"></i> Sign in with Facebook</button>
                   </div>
                 </div>
 
@@ -97,19 +97,19 @@ class Buttons extends React.Component{
                 <div className="row">
                   <div className ="align-button " >
                   <h2> sharp icon AND text </h2>
-                    <button class="btn btn-primary button-icon-sharpEdge-style"><i class="fa fa-facebook-f mr-2"></i></button>
+                    <button class="btn btn-primary button-shadow button-icon-sharpEdge-style"><i class="fa fa-facebook-f mr-2"></i></button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> circular icon AND text </h2>
-                    <button class="btn btn-primary button-icon-rounded-style"><i class="fa fa-facebook-f mr-2"></i></button>
+                    <button class="btn btn-primary button-shadow button-icon-rounded-style"><i class="fa fa-linkedin mr-2"></i></button>
                   </div>
                 </div>
                 <div className="row">
                   <div className ="align-button " >
                   <h2> soft edge</h2>
-                    <button class="btn btn-primary button-icon-softEdge-style"><i class="fa fa-facebook-f mr-2"></i></button>
+                    <button class="btn btn-primary button-shadow button-icon-softEdge-style"><i class="fa fa-twitter mr-2"></i></button>
                   </div>
                 </div>
                
--- src/layout/video_galary/index.js
+++ src/layout/video_galary/index.js
@@ -19,11 +19,51 @@ class Video_galary extends React.Component{
       }
 
 }
+componentDidMount() {
+    window.$(document).ready(function() {
+        var $videoSrc;  
+        window.$('.video-btn').click(function() {
+            $videoSrc = window.$(this).data( "src" );
+        });
+        console.log($videoSrc);
+        // when the modal is opened autoplay it  
+        window.$('#myModal').on('shown.bs.modal', function (e) {
+        // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+        window.$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+        })
+        // stop playing the youtube video when I close the modal
+        window.$('#myModal').on('hide.bs.modal', function (e) {
+            // a poor man's stop video
+            window.$("#video").attr('src',$videoSrc); 
+        }) 
+        // document ready  
+        });
+        
+  }
+  openModal = (event) => {
+    this.setState({isOpen: "block" ,coverDiv:"none"});
+    this._target.playVideoAt();
+  };
+   
+  hideModal = (event) => {
+    console.log("in hide")
+    this.setState({isOpen: "none",coverDiv:"block"});
+    this._target.pauseVideo();
+  };
+  
+  videoOnReady(event) {
+    this._target = event.target;
+    
+  }
 
 render(){
   let visiblity = this.state.visiblity; 
+  let coverDiv=this.state.coverDiv
       return(
         <div className="layout">
+
+            {/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<circle>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+
             <div className="row" style={{display:visiblity}}>
                 <ul style={{listStyleType:"none"}}>
                     <li>
@@ -39,21 +79,29 @@ render(){
             </div>
             <div className="row">
                 <div className="col-lg-8 col-xs-12 col-sm-12 col-md-12  col-style" >
-                <ul style={{listStyleType:"none"}}>
-                    <li>
-                        <div className="video-grid-circle">
-                            <img className="video-grid-circle-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="video-grid-circle">
-                            <img className="video-grid-circle-style" src="https://img.freepik.com/free-photo/video-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
-                        </div>
-                    </li>
-                </ul>
+                    <ul style={{listStyleType:"none"}}>
+                        <li>
+                            <div className='video-grid-circle' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://www.youtube.com/embed/9Yam5B_iasY" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-circle-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='video-grid-circle' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-circle-style" src='https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003'/>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-                
             </div>
+
+{/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<softCorner>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
+
             <div className="row" style={{display:visiblity}}>
                 <ul style={{listStyleType:"none"}}>
                     
@@ -64,7 +112,6 @@ render(){
                     </li>
                     <li>
                         <div className="video-grid-softCorner">
-                            {/* <img className="video-grid-softCorner-style" src="https://img.freepik.com/free-photo/video-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
                         </div>
                     </li>
                 </ul>
@@ -74,18 +121,26 @@ render(){
                     <ul style={{listStyleType:"none"}}>
                         
                         <li>
-                            <div className="video-grid-softCorner">
-                                <img className="video-grid-softCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                            <div className='video-grid-softCorner' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-softCorner-style" src='http://lorempixel.com/output/cats-q-c-640-480-1.jpg'/>
+                                </a>
                             </div>
                         </li>
                         <li>
-                            <div className="video-grid-softCorner">
-                                <img className="video-grid-softCorner-style" src="https://img.freepik.com/free-photo/video-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                            <div className='video-grid-softCorner' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-softCorner-style" src='https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003'/>
+                                </a>
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
+
+{/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<sharpCorner>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
 
             <div className="row" style={{display:visiblity}}>
                 <ul style={{listStyleType:"none"}}>
@@ -96,7 +151,6 @@ render(){
                     </li>
                     <li>
                         <div className="video-grid-sharpCorner">
-                            {/* //<img className="video-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/video-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
                         </div>
                     </li>
                 </ul>
@@ -105,19 +159,27 @@ render(){
             <div className="col-lg-8 col-xs-12 col-sm-12 col-md-12 col-style" >
                 <ul style={{listStyleType:"none"}}>
                     <li>
-                        <div className="video-grid-sharpCorner">
-                            <img className="video-grid-sharpCorner-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
+                        <div className='video-grid-sharpCorner' style={{position:"relative",display:coverDiv}} >
+                            <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal" >
+                                <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                <img className="video-grid-sharpCorner-style" src='https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003'/>
+                            </a>
                         </div>
                     </li>
                     <li>
-                        <div className="video-grid-sharpCorner">
-                            <img className="video-grid-sharpCorner-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
+                        <div className='video-grid-sharpCorner' style={{position:"relative",display:coverDiv}} >
+                            <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal" >
+                                <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                <img className="video-grid-sharpCorner-style" src='https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg'/>
+                            </a>
                         </div>
+                        
                     </li>
                 </ul>
             </div>
             </div>
 
+{/* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<one-below-other>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> */}
 
             <div className="row" style={{display:visiblity}}>
                 <ul style={{listStyleType:"none",paddingLeft: "28px" }}>
@@ -128,30 +190,52 @@ render(){
                     </li>
                     <li>
                         <div className="video-grid-one-below-other">
-                            {/* //<img className="video-grid-one-below-other-style" src="https://img.freepik.com/free-photo/video-human-brain_99433-298.jpg?size=626&ext=jpg" ></img> */}
                         </div>
                     </li>
                 </ul>
-            </div>
-            <div className="row">
-            <div className="col-lg-8 col-xs-12 col-sm-12 col-md-12 col-style" >
-                <ul className="ul-video-grid-one-below-other-style" style={{listStyleType:"none" }}>
-                    <li>
-                        <div className="video-grid-one-below-other">
-                            <img className="video-grid-one-below-other-style" src="https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003" ></img>
-                        </div>
-                    </li>
-                    <li>
-                        <div className="video-grid-one-below-other">
-                            <img className="video-grid-one-below-other-style" src="https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg" ></img>
-                        </div>
-                    </li>
-                </ul>
-            </div>
             </div>
 
-            
-            
+            <div className="row">
+                <div className="col-lg-8 col-xs-12 col-sm-12 col-md-12 col-style" >
+                    <ul className="ul-video-grid-one-below-other-style" style={{listStyleType:"none" }}>
+                        <li>
+                            <div className='video-grid-one-below-other' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://player.vimeo.com/video/58385453?badge=0" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-one-below-other-style" src='https://img.freepik.com/free-photo/image-human-brain_99433-298.jpg?size=626&ext=jpg'/>
+                                </a>
+                            </div>
+                        </li>
+                        <li>
+                            <div className='video-grid-one-below-other' style={{position:"relative",display:coverDiv}} >
+                                <a className=" video-btn" onClick={this.put_bg_color} data-toggle="modal" data-src="https://www.youtube.com/embed/9Yam5B_iasY" data-target="#myModal" >
+                                    <i  className=" fa fa-play fa-2x " style={{color:"white",position:"absolute",left: "45%",top: "45%",zIndex:"999"}}></i>
+                                    <img className="video-grid-one-below-other-style" src='https://res.cloudinary.com/fleetnation-static/image/asset/s--MifwX3dl--/f_auto/header-2018-2560w-c2f0f28235c4f87790c9d5181036c003'/>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content modal-position" >
+
+                            <div class="modal-body">
+
+                            <button type="button"  class="close" data-dismiss="modal" aria-label="Close">
+                            <span  aria-hidden="true">&times;</span>
+                            </button>
+                            {/* <!-- 16:9 aspect ratio --> */}
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="" id="video" allowscriptaccess="always" allow="autoplay"></iframe>
+                            </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </div>
       )
   }
